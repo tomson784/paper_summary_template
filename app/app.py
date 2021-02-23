a5eaf4203d26eeb703773d484720441637af5261
tomson784 <tomohito040759@gmail.com>
2021-02-24 00:00:14 +0900

    update

--- app/app.py
+++ app/app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, request, render_template,\
+                  redirect, url_for, jsonify
 
 app = Flask(__name__)
-
+app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+@app.route("/data")
+def data():
+    r = {"name": "a", "article": "b"}
+    return jsonify(r)
+
+if __name__ == "__main__":
+    app.run()
