a6e14dfcf3fba3e0ac672dc5b000987a592d4a61
tomson784 <tomohito040759@gmail.com>
2021-02-27 21:12:23 +0900

    update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, request, render_template,\
                   redirect, url_for, jsonify
+import glob
+import os
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+articles_dir = "./resources"
+
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    # articles =glob.glob(articles_dir)
+    articles = os.listdir(path=articles_dir)
+    print(articles)
+    return render_template("index.html", articles=articles)
 
 @app.route("/data")
 def data():
