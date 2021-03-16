737e58fcce60158ca4488fe85329a164f6a49ac8
tomson784 <tomohito040759@gmail.com>
2021-03-17 00:42:12 +0900

    update

---
+++ app/utils/category.py
@@ -0,0 +1,16 @@
+import glob
+import os
+
+class Category():
+    def __init__(self, category_name):
+        self.category_name = category_name
+        self.articles = []
+        self.articles_path = []
+    
+    def lists(self, articles):
+        for article_path in articles:
+            article_category = os.path.basename(os.path.dirname(article_path))
+            article_name = os.path.basename(article_path)
+            if (article_category == self.category_name):
+                self.articles.append(article_name)
+                self.articles_path.append(article_path)
