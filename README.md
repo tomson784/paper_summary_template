5a1302e4338811873c40a9246d0383a797bf15f7
tomson784 <tomohito040759@gmail.com>
2021-03-16 17:49:33 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # paper_summary_template
+
+論文の概要をまとめるためのWebアプリテンプレート
+
+## 使い方
+
+Flaskで実行
+
+```
+python app.py
+```
+
+静的サイトとしてビルド
+
+```
+python build.py
+```
--- app/app.py
+++ app/app.py
@@ -34,19 +34,37 @@ def home():
     articles = glob.glob(articles_dir + "/*/*")
     for i in range(len(category_names)):
         categories_class[i].lists(articles)
-    print(categories_class)
-    print(categories_class[0].articles)
-    print(categories_class[0].articles_path)
-    print(articles)
+    # print(categories_class)
+    # print(categories_class[0].articles)
+    # print(categories_class[0].articles_path)
+    # print(articles)
     return render_template("index.html", categories=categories_class)
 
-@app.route('/<category>/<article>')
+@app.route('/<category>/<article>.html')
 def show_article(category, article):
-    path = articles_dir + category + "/" + article
+    path = articles_dir + category + "/" + article + ".md"
     with open(path, "r", encoding="utf-8") as f:
         text = f.read()
-    html = markdown.markdown(text)
-    print(html)
+    html = """
+    <!DOCTYPE html>
+    <html lang="ja">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>論文</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body>
+    """
+    html += markdown.markdown(text)
+    html += """
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    """
+    # print(html)
     # return render_template("article.html", contents=html)
     return html
 
